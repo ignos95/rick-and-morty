bcc775a3378524e4fba1ef6d8a4d0d2859a57018
ignos95 <ignos.c.a@gmail.com>
2020-06-24 23:22:11 +0530

    Updates

---
+++ precache-manifest.823ec8830064523e4eac78d8c56c2900.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47075290d34f984794a865f0df7d05bc",
+    "url": "/ignos95/rick-and-morty.git/index.html"
+  },
+  {
+    "revision": "d846a7c003b956ff95c2",
+    "url": "/ignos95/rick-and-morty.git/static/css/2.e9caa6a3.chunk.css"
+  },
+  {
+    "revision": "7d05d778c758d7413122",
+    "url": "/ignos95/rick-and-morty.git/static/css/main.af8ef545.chunk.css"
+  },
+  {
+    "revision": "d846a7c003b956ff95c2",
+    "url": "/ignos95/rick-and-morty.git/static/js/2.e868b25d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ignos95/rick-and-morty.git/static/js/2.e868b25d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d05d778c758d7413122",
+    "url": "/ignos95/rick-and-morty.git/static/js/main.ae5c3aff.chunk.js"
+  },
+  {
+    "revision": "226f43b7bcbf94295d52",
+    "url": "/ignos95/rick-and-morty.git/static/js/runtime-main.5c5d6cfa.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/ignos95/rick-and-morty.git/static/media/Rick_and_Morty_logo.42d6efdb.png"
+  }
+]);
